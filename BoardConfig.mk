3d9579c6a32204849866237ff932c2afce9f3e40
Abobo7 <2557826346@qq.com>
2026-09-07 07:51:16 +0800

    Fix KERNEL_TOOLCHAIN: must point at the bin/ directory
    kernel.mk joins KERNEL_TOOLCHAIN/; without
    /bin the resolved path misses the binaries and ccache execv fails.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -42,7 +42,7 @@ TARGET_KERNEL_SOURCE := kernel/xiaomi/mocha
 TARGET_KERNEL_CONFIG := mocha_user_defconfig
 # This kernel only boots with the linaro 4.9.4 toolchain
 TARGET_KERNEL_CROSS_COMPILE_PREFIX := arm-linux-gnueabihf-
-KERNEL_TOOLCHAIN := prebuilts/gcc/linux-x86/arm/gcc-linaro-4.9.4-2017.01
+KERNEL_TOOLCHAIN := prebuilts/gcc/linux-x86/arm/gcc-linaro-4.9.4-2017.01/bin
 BOARD_KERNEL_IMAGE_NAME := zImage
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset $(BOARD_RAMDISK_OFFSET) --tags_offset $(BOARD_KERNEL_TAGS_OFFSET)
 BOARD_CUSTOM_BOOTIMG_MK := device/xiaomi/mocha/mkbootimg.mk
